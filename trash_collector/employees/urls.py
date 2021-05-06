220197046af32dd358c0b3483b1bf63be55f3485
Mike Peterson <mpeterson2004@gmail.com>
2021-05-06 12:11:11 -0600

    created user path linked to customers.urls, and employee path linked to employees.url

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
 
 # TODO: Determine what distinct pages are required for the customer user stories, add a path for each in urlpatterns
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('user/', include('customers.urls')),
+    path('employee/', include('employees.url'))
 ]
