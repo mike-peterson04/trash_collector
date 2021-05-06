c7454ce7756f71edb0ca88bc4cef072351c8cf25
Mike Peterson <mpeterson2004@gmail.com>
2021-05-06 10:55:03 -0600

    first draft of employee class

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-# TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    area = models.CharField(max_length=15)
