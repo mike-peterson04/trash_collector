4a4b6300a36958240ee71ab9bad7b2c9b4aaae28
Mike Peterson <mpeterson2004@gmail.com>
2021-05-06 14:56:04 -0600

    Added logic to allow creation of user on onboarding page for customers

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -10,7 +10,7 @@ class Customer(models.Model):
     pickup_day = models.CharField(max_length=10, blank=True, null=True)
     one_time_pickup = models.DateField(blank=True, null=True)
     account_balance = models.FloatField(default=0.00)
-    suspension = models.BooleanField(False, blank=True, null=True)
+    suspension = models.BooleanField(default=False)
     suspension_end = models.DateField(blank=True, null=True)
     zipcode = models.CharField(max_length=15, blank=True, null=True)
     address = models.CharField(max_length=50, blank=True, null=True)
