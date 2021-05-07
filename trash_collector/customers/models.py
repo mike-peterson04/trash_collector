a169e505fb1fd0b0a0907b67c1926aa7113aabe5
Mike Peterson <mpeterson2004@gmail.com>
2021-05-07 13:47:14 -0600

    logic for suspend page to be merged with partner's html

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 # Create your models here.
 
-# TODO: Finish customer model by adding necessary properties to fulfill user stories
-
-
 class Customer(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
@@ -11,6 +8,7 @@ class Customer(models.Model):
     one_time_pickup = models.DateField(blank=True, null=True)
     account_balance = models.FloatField(default=0.00)
     suspension = models.BooleanField(default=False)
+    suspension_start = models.DateField(blank=True, null=True)
     suspension_end = models.DateField(blank=True, null=True)
     zipcode = models.CharField(max_length=15, blank=True, null=True)
     address = models.CharField(max_length=50, blank=True, null=True)
