95b5739efce6d331df2e8b52a79236ee0f6d401d
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-10 16:09:43 -0500

    got map to render in schedule page

---
+++ trash_collector/employees/static/employees/map.js
@@ -0,0 +1,14 @@
+// Initialize and add the map
+let map;
+
+function initMap() {
+  const center = { lat: 41.57419394907601, lng: -93.8033035605153 }
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: center,
+    zoom: 15,
+  });
+  const marker = new google.maps.Marker({
+    position: center,
+    map: map,
+  });
+}
