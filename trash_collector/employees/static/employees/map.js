b4cc77078b941bd8ecf1cd0233ab9461a5a92a40
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-11 10:14:55 -0500

    trying to get map to work

--- trash_collector/employees/static/employees/map.js
+++ trash_collector/employees/static/employees/map.js
@@ -1,8 +1,7 @@
-// Initialize and add the map
 let map;
 
 function initMap() {
-  const center = '50131'
+  const center = { lat: 41.57419394907601, lng: -93.8033035605153 }
   map = new google.maps.Map(document.getElementById("map"), {
     center: center,
     zoom: 15,
